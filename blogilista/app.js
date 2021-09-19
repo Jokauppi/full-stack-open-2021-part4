13f7173353b7b66e9c4bc7c52d04b4aa71ff6161
Joona Kauppila <jokauppi@helsinki.fi>
2021-09-19 07:05:43 +0300

    Osa 4 tehtävä 4.2

---
+++ blogilista/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(logger.info('db connected'))
+	.catch(error => logger.info('db connection failed\n' + error))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
