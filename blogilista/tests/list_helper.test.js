2860858f6073811d60d6737c0514311a747869ab
Joona Kauppila <jokauppi@helsinki.fi>
2021-09-20 04:17:35 +0300

    Osa 4 tehtävä 4.4

--- blogilista/tests/list_helper.test.js
+++ blogilista/tests/list_helper.test.js
@@ -6,3 +6,30 @@ test('dummy returns one', () => {
 	const result = listHelper.dummy(blogs)
 	expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+
+	const listWithOneBlog = [
+		{
+			'title': 'example',
+			'author': 'blogger',
+			'url': 'blog.example',
+			'likes': 7,
+			'id': '6147da558b976c1408a0e55a'
+		}
+	]
+
+	test('of empty list is 0', () => {
+		expect(listHelper.totalLikes([])).toBe(0)
+	})
+
+	test('when list has only one blog equals the likes of the single blog', () => {
+		expect(listHelper.totalLikes(listWithOneBlog)).toBe(7)
+	})
+
+	test('of a longer list is correct', () => {
+		expect(listHelper.totalLikes(listWithOneBlog.concat(listWithOneBlog))).toBe(14)
+	})
+
+
+})
--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	const likesSum = (prev, current) => prev + current.likes
+	return blogs.reduce(likesSum, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
