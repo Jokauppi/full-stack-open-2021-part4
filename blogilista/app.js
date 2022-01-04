d70088563a7831ac1d7b573f8cbc63d9678a0c87
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-01-04 10:55:53 +0200

    4.18 Implement token-based authentication

--- blogilista/app.js
+++ blogilista/app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -21,6 +22,7 @@ if (process.env.NODE_ENV !== 'test') app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ blogilista/controllers/login.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user')
+
+const loginRouter = require('express').Router()
+
+loginRouter.post('/', async (request, response) => {
+  const loginData = request.body
+
+  const user = await User.findOne({ username: loginData.username })
+
+  const accessPermitted = user === null ? false : await bcrypt.compare(loginData.password, user.passwordHash)
+
+  if (!user || !accessPermitted) {
+    return response.status(401).json({ error: 'invalid username or password' })
+  }
+
+  const tokenUser = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET)
+
+  response.status(200).send({ name: user.name, username: user.username, token })
+})
+
+
+module.exports = loginRouter
